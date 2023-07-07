dbab75476eb8a6db9ded6594212480403578204f
tehrii <86597038+tehrii@users.noreply.github.com>
2023-07-08 01:43:07 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,25 +23,25 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.owner = ['916909137213'] //ur owner number
 global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ownername = "TEHRII" //ur owner name
+global.ytname = "YT:Tehri" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.ownernumber = '923165563235'
+global.ownername = 'Tehrii'
+global.ownerNumber = ["923165563235@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "Sticker By Tehri"
+global.author = "tehri\n\n+923165563235"
+global.creator = "923165563235@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
